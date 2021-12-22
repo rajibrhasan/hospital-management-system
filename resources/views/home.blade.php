e04ec787961e5d87a793ab0307c99031cfe9282c
Rakibul Hasan Rajib <1921834@iub.edu.bd>
2021-12-22 14:13:34 +0600

    Modified Last one

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,5 +114,32 @@
 @endsection('content')
 
 @section('footer')
-  @parent
+  <section style="background-color:#316387">
+      <div class="container text-white">
+        <div class="row py-5 px-3">
+          <div class="col-lg-4 ">
+            <h5 class="mb-4">Contact Us</h5>
+            <p>House #16, Road # 2, Dhanmondi R/A, Dhaka-1205, Bangladesh<br>
+              Phone : 09613 787801, 09666 787801<br>
+              E-mail : info@populardiagnostic.com
+            </p>
+          </div>
+
+          <div class="col-lg-4">
+            <h5 class="mb-4">Important Links</h5>
+            <p>House #16, Road # 2, Dhanmondi R/A, Dhaka-1205, Bangladesh<br>
+              Phone : 09613 787801, 09666 787801<br>
+              E-mail : info@populardiagnostic.com
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <h5 class="mb-4"> Quick Links</h5>
+            <p>House #16, Road # 2, Dhanmondi R/A, Dhaka-1205, Bangladesh<br>
+              Phone : 09613 787801, 09666 787801<br>
+              E-mail : info@populardiagnostic.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </section> 
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -29,7 +29,12 @@
 
     
     
+
+    @yield('footer')
+   
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    
      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js">
