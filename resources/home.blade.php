dc87530a3821c2515988087cb227d9ab515cc219
SumyiaAfnanMukta <sumyiaamukta@gmail.com>
2021-12-24 02:20:01 +0400

    picture added

--- resources/home.blade.php
+++ resources/home.blade.php
@@ -58,7 +58,7 @@
       </div>
     </nav>
      <section>
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="max-height: 50%;" >
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -68,8 +68,9 @@
           <div class="carousel-item active">
             <img src="{{'images/car1.jpg'}}" class="d-block w-100" style="height: 500px;" alt="...">
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item" >
             <img src="{{'images/car2.png'}}" class="d-block w-100" style="height: 500px;" alt="...">
+            
           </div>
           <div class="carousel-item">
             <img src="{{'images/car3.jpg'}}" class="d-block w-100" style="height: 500px;" alt="...">
