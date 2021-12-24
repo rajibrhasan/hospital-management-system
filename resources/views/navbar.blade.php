75a38fc31f29aa5faab3bd3311f599c44ad0c546
SumyiaAfnanMukta <sumyiaamukta@gmail.com>
2021-12-24 23:09:09 +0400

    ...

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color:#364bb5;">
   <div class="container">
-    <a class="navbar-brand" href="#">Lifeline Hospital</a>
+    <a class="navbar-brand" href="#"><strong>LH</strong></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
       <span class="navbar-toggler-icon"></span>
     </button>
