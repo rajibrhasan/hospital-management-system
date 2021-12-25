1da19e6c1457931ec774655a687f8c1e8afe0962
SumyiaAfnanMukta <sumyiaamukta@gmail.com>
2021-12-25 12:17:52 +0400

    gallery added

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('header')
+@include('navbar')
+
+@endsection('header')
+
+@section('content')
+
+
+
+
+@endsection('content')
+
+@section('footer')
+@include('footer')
+@endsection('footer')
