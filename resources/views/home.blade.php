886663b145d729bf4e4bcd90aeed1a70c8f850de
Rakibul Hasan Rajib <1921834@iub.edu.bd>
2021-12-25 12:47:14 +0600

    Homepage Added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,16 @@
 @section('content')
 
 <section>
+  <div class="container">
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-inner" style="height:500px;" >
+    <div class="carousel-inner ">
       <div class="carousel-item active">
-        <img src="{{'images/car1.jpg'}}" class="d-inline-block h-50" style="width: 100%" alt="...">
+        <img src="{{'images/car1.jpg'}}" class="d-inline-block h-50" style="width: 100%;height:500px;" alt="...">
       </div>
       <div class="carousel-item">
         <img src="{{'images/car2.png'}}" class="d-inline-block  h-50"  style="width: 100%"  alt="...">
@@ -33,7 +34,8 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
-  </div>    
+  </div>  
+  </div>  
 </section>
 
 <section class="container">
